fcb50659ff1dc50f518e2ceed577a4e1520533ed
Bartosz <borkowski.bartosz@gmail.com>
2020-08-11 22:07:09 +0200

    Comment model updated

--- blog/models.py
+++ blog/models.py
@@ -6,15 +6,18 @@ STATUS = ((0, 'Published'),
             (1, 'Draft'))
 
 class Post(models.Model):
-    title = models.CharField(max_length=200, unique=True),
-    slug = models.SlugField(max_length=150, unique=True),
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=150, unique=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
     edited_on = models.DateTimeField(auto_now= True)
     created_on = models.DateTimeField(auto_now_add=True)
-    content = models.TextField(verbose_name=Post content),
+    content = models.TextField(verbose_name='Post content')
     status = models.IntegerField(choices=STATUS, default=1)
     def __str__(self):
         return self.title
 
 class Comment(models.Model):
-    pass
+    rating = models.IntegerField()
+    content = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comment')
+    created_on = models.DateTimeField(auto_now_add=True)   
