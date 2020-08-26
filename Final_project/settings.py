c00cf03435631c2e07496eeb5ed11aaaaec5a054
Bartosz <borkowski.bartosz@gmail.com>
2020-08-26 21:06:13 +0200

    initial heroku2

--- Final_project/settings.py
+++ Final_project/settings.py
@@ -16,7 +16,7 @@ import dotenv
 from pathlib import Path
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 
 
 # Quick-start development settings - unsuitable for production
