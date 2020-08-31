4943bfdeb106bfd0342ab79b49d38ca4764e9ca8
Bartosz <borkowski.bartosz@gmail.com>
2020-08-31 21:44:25 +0200

    s3 test3

--- Final_project/settings.py
+++ Final_project/settings.py
@@ -177,11 +177,12 @@ AWS_S3_OBJECT_PARAMETERS = {
 }
 AWS_LOCATION = 'static'
 AWS_S3_REGION_NAME = "us-east-2"
-# STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 STATICFILES_LOCATION = 'static'
 MEDIAFILES_LOCATION = "media"
-STATICFILES_STORAGE = 'custom_storages.StaticStorage'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+# STATICFILES_STORAGE = 'custom_storages.StaticStorage'
 DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
+
 STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
 MEDIA_URL = 'http://' + AWS_STORAGE_BUCKET_NAME + '.s3.amazonaws.com/media/'
 
--- custom_storages.py
+++ custom_storages.py
@@ -1,7 +1,5 @@
 from django.conf import settings
 from storages.backends.s3boto3 import S3Boto3Storage
-import os
-
 
 
 class StaticStorage(S3Boto3Storage):
@@ -22,7 +20,8 @@ class StaticStorage(S3Boto3Storage):
 class MediaStorage(S3Boto3Storage):
 
     location = settings.MEDIAFILES_LOCATION
-
+    file_overwrite = False
+    
     def _clean_name(self, name):
         return name
 
