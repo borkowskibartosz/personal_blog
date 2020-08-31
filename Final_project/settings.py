5a0b47355cadda0db45e2d264cb7aaf50e42bb04
Bartosz <borkowski.bartosz@gmail.com>
2020-08-31 14:36:35 +0200

    s3 test3

--- Final_project/settings.py
+++ Final_project/settings.py
@@ -182,8 +182,8 @@ STATICFILES_LOCATION = 'static'
 MEDIAFILES_LOCATION = "media"
 STATICFILES_STORAGE = 'custom_storages.StaticStorage'
 DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
-# STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
-# MEDIA_URL = 'http://' + AWS_STORAGE_BUCKET_NAME + '.s3.amazonaws.com/media/'
+STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+MEDIA_URL = 'http://' + AWS_STORAGE_BUCKET_NAME + '.s3.amazonaws.com/media/'
 
 # DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
