8b783890c9f627450b758d038de44ec95eb6d7c7
Bartosz <borkowski.bartosz@gmail.com>
2020-08-31 08:24:29 +0200

    s3 test2

--- Final_project/settings.py
+++ Final_project/settings.py
@@ -172,6 +172,7 @@ AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = 'obscuretemplebucket'
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3BotoStorage'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+AWS_S3_REGION_NAME = "us-east-2"
 STATIC_URL = 'http://' + AWS_STORAGE_BUCKET_NAME + '.s3.amazonaws.com/static/'
 MEDIA_URL = 'http://' + AWS_STORAGE_BUCKET_NAME + '.s3.amazonaws.com/media/'
 
