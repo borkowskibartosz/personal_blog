789fa7601e5948e113d37011d33c81e2c7e58888
Bartosz <borkowski.bartosz@gmail.com>
2020-09-01 19:59:38 +0200

    s3 test7

--- Final_project/settings.py
+++ Final_project/settings.py
@@ -202,7 +202,7 @@ STATICFILES_DIRS = [
     BASE_DIR.joinpath("static"),
 ]
 
-MEDIA_URL = '/media/'
+# MEDIA_URL = '/media/'
 MEDIA_ROOT= os.path.join(BASE_DIR, 'media')
 
 AUTH_PROFILE_MODULE = 'blog.UserProfile'
