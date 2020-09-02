b2321777de370faf89d26bb810bc2711fb2f9ab0
Bartosz <borkowski.bartosz@gmail.com>
2020-09-02 18:56:33 +0200

    s3 test10

--- blog/models.py
+++ blog/models.py
@@ -35,7 +35,9 @@ class Photo(models.Model):
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    avatar = models.ImageField(upload_to="profile/", blank=True)
+    # avatar = models.ImageField(upload_to="profile/", blank=True)
+    avatar = models.ImageField(blank=True)
+
     email_confirmed = models.BooleanField(default=False)
     reset_password = models.BooleanField(default=False)
     def __str__(self):  
