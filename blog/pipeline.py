f6973944c66743e5084ffb09ab60f9f8cdb374ac
Bartosz <borkowski.bartosz@gmail.com>
2020-09-02 21:21:17 +0200

    s3 test11

--- blog/pipeline.py
+++ blog/pipeline.py
@@ -1,6 +1,7 @@
 from .models import UserProfile
 from django.core.files import File
 from django.conf import settings
+import boto3
 import urllib.request
 import requests
 import os
@@ -13,9 +14,18 @@ def get_avatar(backend, strategy, details, response,
     if backend.name == 'google-oauth2':
         url = response['picture']
     if url:
-        r = requests.get(url)
-        with open(settings.MEDIA_ROOT + f"/profile/{user.username}.jpg", "wb") as f:
-            f.write(r.content)
+        s3 = boto3.resource(
+            's3',
+            region_name='us-east-2',
+            aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
+            aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY,
+        )
+        s3.Object(settings.AWS_STORAGE_BUCKET_NAME, f'{user.username}.jpg').put(Body=open(url, 'rb'))
+
+        # r = requests.get(url)
+        # with open(settings.MEDIA_ROOT + f"/profile/{user.username}.jpg", "wb") as f:
+        #     f.write(r.content)
+
         user_profile = UserProfile.objects.get(user=user)
-        user_profile.avatar = f"/profile/{user.username}.jpg"
+        user_profile.avatar = f"/{user.username}.jpg"
         user_profile.save()
