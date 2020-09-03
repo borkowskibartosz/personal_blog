7713953c76b11275c8d30874ae4b812150f407a2
Bartosz <borkowski.bartosz@gmail.com>
2020-09-03 08:28:28 +0200

    s3 test12

--- blog/pipeline.py
+++ blog/pipeline.py
@@ -1,6 +1,8 @@
 from .models import UserProfile
 from django.core.files import File
 from django.conf import settings
+from urllib.request import urlopen
+from tempfile import NamedTemporaryFile
 import boto3
 import urllib.request
 import requests
@@ -14,19 +16,38 @@ def get_avatar(backend, strategy, details, response,
     if backend.name == 'google-oauth2':
         url = response['picture']
     if url:
-        s3 = boto3.resource(
-            's3',
-            region_name='us-east-2',
-            aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
-            aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY,
-        )
-        r = requests.get(url)
-        s3.Object(settings.AWS_STORAGE_BUCKET_NAME, f'{settings.MEDIAFILES_LOCATION}/{user.username}.jpg').put(Body=r.content)
+        #s3boto
+        # s3 = boto3.resource(
+        #     's3',
+        #     region_name='us-east-2',
+        #     aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
+        #     aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY,
+        # )
+
+        # r = requests.get(url)
+        # s3.Object(settings.AWS_STORAGE_BUCKET_NAME, f'{settings.MEDIAFILES_LOCATION}/{user.username}.jpg').put(Body=r.content)
+        #/s3boto
+
         # s3.Bucket('bucketname').upload_file('/local/file/here.txt','folder/sub/path/to/s3key')
+        #local
         # r = requests.get(url)
         # with open(settings.MEDIA_ROOT + f"/profile/{user.username}.jpg", "wb") as f:
         #     f.write(r.content)
+        #/local
+        # r = requests.get(url)
+        # with open(settings.MEDIA_ROOT + f"/profile/{user.username}.jpg", "wb") as f:
+        #     f.write(r.content)
+        # user_profile = UserProfile.objects.get(user=user)
+        # user_profile.avatar = f"/{user.username}.jpg"
+        # user_profile.save()
 
+        #tutu
+        img_temp = NamedTemporaryFile(delete=True)
+        img_temp.write(urlopen(url).read())
+        img_temp.flush()
         user_profile = UserProfile.objects.get(user=user)
-        user_profile.avatar = f"/{user.username}.jpg"
-        user_profile.save()
+        user_profile.avatar.save(f"{user.username}", File(img_temp))                                
+
+        # user_profile = UserProfile.objects.get(user=user)
+        # user_profile.avatar = f"/{user.username}.jpg"
+        # user_profile.save()
